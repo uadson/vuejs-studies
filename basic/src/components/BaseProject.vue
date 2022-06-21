d3d32bb3dca31733e76a212a098c86fb6bee67e7
Uadson Feitosa <uadsoncastro@gmail.com>
2022-06-21 18:33:27 -0300

    returning data on template throught function data()

--- basic/src/components/BaseProject.vue
+++ basic/src/components/BaseProject.vue
@@ -1,9 +1,26 @@
 <template>
-    <h1>This is BaseProject.vue</h1>
+    <div>
+        <h1>This is Vue JS</h1><hr>
+        <h3>Introdution</h3>
+        <p>Hello, my name is {{ name }} and I'm studing {{ framework[0] }}.<br>
+           I'm {{ area }} {{ occupation }} and I programing with {{ language }}<br>
+           and framework {{ framework[1]}}.
+        </p>
+    </div>
+    
 </template>
 
 <script>
 export default {
-    name: 'BaseProject'
+    name: 'BaseProject',
+    data() {
+        return {
+            name: "Uadson",
+            framework: ["Vue JS", "Django"],
+            occupation: "Web Developer",
+            area: "Backend",
+            language: "Python",
+        }
+    }
 }
 </script>
